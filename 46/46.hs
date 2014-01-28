d6ef92bfbfce2dab9f62b9605a9e7917db81c2bb
Jesús Espino <jesus.espino@kaleidos.net>
2014-01-28 22:53:14 +0100

    Improved 46 solution

--- 46/46.hs
+++ 46/46.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 and' :: Bool -> Bool -> Bool
 and' True True = True
 and' _ _ = False
@@ -7,7 +9,5 @@ or' True _ = True
 or' _ True = True
 or' _ _ = False
 
-table :: (Bool -> Bool -> Bool) -> IO String
-table f = do
-    return $ foldr (++) "" [ foldr (++) " " [show x, show y, show $ f x y] | x <- [True,False], y <- [True, False]]
-
+table :: (Bool -> Bool -> Bool) -> String
+table f = unlines [ intercalate " " [show x, show y, show $ f x y] | x <- [True,False], y <- [True, False]]
